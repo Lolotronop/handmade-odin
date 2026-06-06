ca37f43e94c0e602a65a8aa5aca91d6f48f5a4fd
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-06-07 02:27:53 +0700

    chore: actually rebuild the project on leader c

--- nvim.lua
+++ nvim.lua
@@ -1,14 +1,14 @@
 vim.keymap.set('n', '<leader>c', function()
-    vim.notify("Building...")
-    vim.fn.system({ 'odin', 'build', '.', '--debug' })
-    vim.notify("Finished build")
+    local time = vim.loop.hrtime()
+    vim.system({ 'odin', 'build', '.', '--debug' }, nil, function()
+        local elapsed = vim.loop.hrtime() - time
+        vim.notify("Build in " .. elapsed / 1000000 .. " ms")
 
-    vim.notify("Killing...")
-    vim.fn.system({ "D:/soft/raddbg/raddbg.exe", "--ipc", "kill_all" })
-    vim.notify("Finished Killing")
-
-
-    vim.notify("Running...")
-    vim.fn.system({ "D:/soft/raddbg/raddbg.exe", "--ipc", "run" })
-    vim.notify("Ran")
+        vim.system({ "D:/soft/raddbg/raddbg.exe", "--ipc", "kill_all" }, nil, function()
+            vim.notify("Killed")
+            vim.system({ "D:/soft/raddbg/raddbg.exe", "--ipc", "run" }, nil, function()
+                vim.notify("Running")
+            end)
+        end)
+    end)
 end, { desc = "Compile and run thru the raddbg debugger" })
